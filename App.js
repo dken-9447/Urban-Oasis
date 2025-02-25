c57449abad71d2ba68a6d28b12858eefdf50a054
dken-9447 <dken@mail.valenciacollege.edu>
2025-02-25 16:16:51 -0500

    2/25/2025 @ 1614 - Added React Navigation to connect the other pages - npm installed dependencies and packages that are relevant to React Navigation. Details can be seen here- https://reactnavigation.org/docs/stack-navigator - Fixed the App.js page to include the extra pages - Made the foods.screens.js page a blank canvas for George to add code to.

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { HomeScreen } from "./src/features/restaurants/screens/home.screen";
+import { FoodsScreen } from "./src/features/restaurants/screens/foods.screens";
+import { NavigationContainer } from "@react-navigation/native";
 import "./global.css";
+import "./gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        <>
-            {/*
-             <FoodsScreen /> 
-             */}
-            <HomeScreen />
-            <ExpoStatusBar style="auto" />
-        </>
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Foods" component={FoodsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
--- src/features/restaurants/screens/foods.screens.js
+++ src/features/restaurants/screens/foods.screens.js
@@ -1,30 +1,83 @@
 import React from "react";
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
-import { Card, Paragraph, Title } from "react-native-paper";
+import {
+    StatusBar,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Platform
+} from "react-native";
+import { Image } from "react-native";
+import { IconButton } from "react-native-paper";
+import { useState } from "react";
 
-export const FoodsScreen = () => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar />
-        </View>
-        <View style={styles.list}>
-            <Text>Welcome to the Fresh Side of Life!</Text>
-        </View>
-        <View>
-            <Card>
-                <Card.Cover source={require("../../../../assets/urban-oasis-card.png")} />
-                <Card.Content>
-                    <Title>Urban Oasis</Title>
-                    <Paragraph>lorem ipsum</Paragraph>
-                </Card.Content>
-            </Card>
-        </View>
-        <View style={styles.bottomnNav}>
-            <Text>Placeholder for bottom navbar</Text>
-        </View>
-    </SafeAreaView>
-);
+export const FoodsScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View className="absolute top-0 left-0 right-0 flex-row items-center justify-between py-4 px-6 bg-[#467e53]">
+                {/* Hamburger Menu Icon */}
+                <IconButton
+                    icon="menu"
+                    size={24}
+                    onPress={() =>
+                        console.log("Hamburger Menu has been pressed // Used for debugging")
+                    }
+                    className="text-white ml-auto"
+                    iconColor="white"
+                />
+
+                {/* Urban Oasis Text Image */}
+                <Image
+                    source={require("../../../../assets/urban-oasis-text-only.png")}
+                    style={{
+                        width: 150,
+                        height: 40,
+                        resizeMode: "contain",
+                        position: "absolute",
+                        left: "55%",
+                        transform: [{ translateX: -75 }]
+                    }}
+                />
+
+                {/* Magnifying glass icon */}
+                <IconButton
+                    icon="magnify"
+                    size={24}
+                    onPress={() =>
+                        console.log("Magnifying button has been pressed // Used for debugging")
+                    }
+                    className="text-white ml-auto"
+                    iconColor="white"
+                />
+            </View>
+
+            {/* Bottom Navigation */}
+            <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between bg-[#467e53] py-4 px-12">
+                <IconButton
+                    icon="basket-outline"
+                    size={28}
+                    onPress={() => navigation.navigate("Home")}
+                    iconColor="white"
+                />
+                <IconButton
+                    icon="home-outline"
+                    size={28}
+                    onPress={() => console.log("Home icon has been pressed")}
+                    iconColor="white"
+                />
+                <IconButton
+                    icon="silverware-fork-knife"
+                    size={28}
+                    onPress={() => navigation.navigate("Foods")}
+                    iconColor="white"
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
     container: { flexGrow: 0.25, marginTop: StatusBar.currentHeight },

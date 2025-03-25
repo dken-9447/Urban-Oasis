e05c0313f85e8f4c6fd656a7210b3e246332a524
psychomonger <133125250+psychomonger@users.noreply.github.com>
2025-03-25 13:17:14 -0400

    Implemented recipe unit testing and minor update to Recipe Detail screen.

---
+++ src/features/restaurants/components/recipeDetail.js
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { Text, View, ScrollView, Image, Button} from "react-native";
+import { useEffect, useState } from "react";
+import { getRecipeDetail } from "./data";
+
+
+// This function mimicks the code used by recipeDetail.screen.js. However
+// all of the formatting code has been removed as that is not being tested.
+export default function RecipesDetail({}) {
+    
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            let fetchedRecipesDetails = await getRecipeDetail();
+            setRecipes(fetchedRecipesDetails);
+        }
+
+        fetchData();
+    });
+
+    return (
+        <View>
+            <ScrollView>
+            {recipes.map((item) => (
+                <View  key={item.id}>
+                    <Text>
+                        {item.title}
+                    </Text>
+                    <View>
+                        <Image source={{ uri: item.imageUrl }} />
+                    </View> 
+                    <Text>
+                        {item.category} • {item.totalTime}
+                    </Text>
+                    <Text>
+                        {item.description}
+                    </Text>
+                    <Text>
+                        Servings: {item.servings}
+                    </Text>
+                    <Text>
+                        Cost: {item.estimatedTotalCost}
+                    </Text>
+                    <Text>
+                        Calories: {item.calories}
+                    </Text>
+                    <Text>
+                        Prep: {item.prepTime} | Cook: {item.cookTime}
+                    </Text>
+                    <Text>
+                        Fat: {item.fat} | Carbs: {item.carbs} | Protein:{" "}
+                        {item.protein}
+                    </Text>
+                    <Text>Notes:</Text>
+                    <Text>
+                        {item.notes}
+                    </Text> 
+                    <Text>Ingredients:</Text>
+                    <Text>{item.ingredients}</Text>
+                    <Text>Directions:</Text>
+                    <Text>{item.directions}</Text>
+                    <Text>Pricing Guide:</Text>
+                    <Text>{item.ingredientCosts}</Text>
+                </View>
+            ))}
+            </ScrollView>
+        </View>
+    );
+}                    
---
+++ src/features/restaurants/components/recipeList.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Text, View, ScrollView, Pressable, Image} from "react-native";
+import { useEffect, useState } from "react";
+import { getRecipeList } from "./data";
+
+// This function mimicks the code used by recipeList.screen.js. However
+// all of the formatting code has been removed as that is not being tested.
+export default function RecipesList({}) {
+    
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            let fetchedRecipes = await getRecipeList();
+            setRecipes(fetchedRecipes);
+        }
+
+        fetchData();
+    });
+
+    return (
+        <View>
+            <ScrollView>
+                {recipes.map((item) => (
+                    <Pressable key={item.id} >
+                        <View>
+                            <View>
+                                <View>
+                                        <Image source={{ uri: item.imageUrl }} />
+                                </View>
+                                <View>
+                                    <Text>{item.title}</Text>
+                                    <View>
+                                        <Text>{item.category}</Text>
+                                        <Text>{item.rating}</Text>
+                                        <Text>{item.totalTime}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </Pressable>
+                ))}
+                </ScrollView>
+        </View>
+    );
+}                    
